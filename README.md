e415d2ba198dd3adea71304e55c488009164ad9d
Adam Natrop <adamnatrop@gmail.com>
2021-07-26 13:11:13 -0500

    pushing assessment code

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lyssn_assesment
+
+This is my coding assesment for the Front End Developer position. 
+
+Requirements: 
+•	List each user, last name first, with each name capped
+•	When a user is hovered over, the containing element must change color
+•	Include a padding of 10px by 5px for each user
+•	The display must be vertically and horizontally centered on the screen, and must remain so during screen resizing
---
+++ assets/js/index.js
@@ -0,0 +1,28 @@
+const listContainer = document.getElementById('userList');
+
+userData = [
+    {"fname":"esme", "lname":"sanchez", "id":1892038, "isTherapist":true}, 
+    {"fname":"rick", "lname":"jeps", "id":1894024, "isTherapist":false}, 
+    {"fname":"angie", "lname":"lange", "id":1982403, "isTherapist":false}, 
+    {"fname":"renat", "lname":"gros", "id":1894039, "isTherapist":true}, 
+    {"fname":"bettino", "lname":"yang", "id":3789204, "isTherapist":true}
+]; 
+
+
+
+function renderUserList() {
+
+    userList = document.createElement('div');
+    userList.setAttribute("id", 'list')
+    listContainer.appendChild(userList);
+
+    userData.forEach(user => {
+        let div = document.createElement('div');
+        div.setAttribute("class", "user");
+        div.textContent = user.lname.charAt(0).toUpperCase() + user.lname.slice(1) + ", " + user.fname.charAt(0).toUpperCase() + user.fname.slice(1);
+        userList.appendChild(div);
+        
+    });
+}
+
+renderUserList();
